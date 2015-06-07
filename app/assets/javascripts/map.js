d5d1b5bca3fe22ff7b0e5058b60c07ccf6a9e04c
mmplisskin <mmplisskin@gmail.com>
2015-06-06 19:26:06 -0700

    removed s3 from development paperclip try 3

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,9 +1,9 @@
 
 google.maps.event.addDomListener(window, 'load', initialize);
-google.maps.event.addDomListener(window, 'page:load', initialize)
-
+google.maps.event.addDomListener(window, 'page:load', initialize);
 
 function initialize() {
+
   var url = window.location.origin + window.location.pathname + ".json";
 
   $.get(url, function(results){
@@ -14,39 +14,33 @@ function initialize() {
     var myCenter = new google.maps.LatLng(lat,long);
     var marker;
 
-  var mapProp = {
+    var mapProp = {
+      center: myCenter,
+      zoom:12,
+      zoomControl:false,
+      panControl:false,
+      mapTypeId:google.maps.MapTypeId.HYBRID
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-    center: myCenter,
-    zoom:12,
-    zoomControl:false,
-    panControl:false,
-    mapTypeId:google.maps.MapTypeId.HYBRID
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var marker = new google.maps.Marker({
+      position:myCenter
+      // animation:google.maps.Animation.BOUNCE
+    });
 
-var marker = new google.maps.Marker({
+    marker.setMap(map);
 
-  position:myCenter,
-  icon: image
+    google.maps.event.addListener(marker,'click', function() {
+      map.setZoom(16);
+      map.setCenter(marker.getPosition());
+    });
 
-  // animation:google.maps.Animation.BOUNCE
-  });
+    var infowindow = new google.maps.InfoWindow({
+      content: my_content
+    });
 
-marker.setMap(map);
+    infowindow.open(map,marker);
 
-google.maps.event.addListener(marker,'click',function() {
-  map.setZoom(16);
-  map.setCenter(marker.getPosition());
-  });
-
-var infowindow = new google.maps.InfoWindow({
-  content: my_content
-  });
-
-infowindow.open(map,marker);
-
-})
-
-
+  })
 }
 google.maps.event.addDomListener(window, 'load', initialize);
